260c079055e0314b3f4e26f939e7481927dc55af
li-sa <lisa.schatt@googlemail.com>
2019-03-26 13:14:08 +0100

    changed loading prolog files to all files in specific directory

--- src/dare2del/logic/prolog/PrologFileLoader.java
+++ src/dare2del/logic/prolog/PrologFileLoader.java
@@ -5,18 +5,25 @@ import org.jpl7.JPL;
 import org.jpl7.Query;
 import org.jpl7.Term;
 
+import java.io.File;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.List;
 
 public class PrologFileLoader {
 
-    private final String CLAUSE_FILE = getClass().getProtectionDomain().getCodeSource().getLocation().getPath() + "/../clauses.pl";
-    private final String RULE_FILE = getClass().getProtectionDomain().getCodeSource().getLocation().getPath() + "/../irrelevanceTheory.pl";
+//    private final String CLAUSE_FILE = getClass().getProtectionDomain().getCodeSource().getLocation().getPath() + "/../PrologFiles/clauses.pl";
+//    private final String RULE_FILE = getClass().getProtectionDomain().getCodeSource().getLocation().getPath() + "/../PrologFiles/irrelevanceTheory.pl";
+
+    private final String PROLOGFILES_DIRECTORY = getClass().getProtectionDomain().getCodeSource().getLocation().getPath() + "../PrologFiles";
 
     public PrologFileLoader() {
         List<String> prologFilesToLoad = new ArrayList<>();
-        prologFilesToLoad.add(CLAUSE_FILE);
-        prologFilesToLoad.add(RULE_FILE);
+        prologFilesToLoad = addAllPrologFiles(prologFilesToLoad);
+
+//        prologFilesToLoad.add(CLAUSE_FILE);
+//        prologFilesToLoad.add(RULE_FILE);
 
         JPL.init();
 
@@ -25,10 +32,22 @@ public class PrologFileLoader {
                     new Atom(eachPrologFile)});
             if (!consultQuery.hasSolution()) {
                 // TODO: Exception Handling
-                //throw new Exception("File not found: " + eachPrologFile);
                 System.out.println("!!! EXCEPTION: No file " + eachPrologFile + " found !!!");
             }
             consultQuery.close();
         }
+    }
+
+    private List<String> addAllPrologFiles(List<String> prologFilesToLoad) {
+        Path prologDirPath = Paths.get(PROLOGFILES_DIRECTORY.replaceFirst("/", ""));
+        File prologDir = prologDirPath.toFile();
+
+        for (File eachFile : prologDir.listFiles()) {
+            if (eachFile.isFile() && eachFile.getName().endsWith(".pl")) {
+                prologFilesToLoad.add(eachFile.getPath());
+            }
+        }
+
+        return prologFilesToLoad;
     }
 }
